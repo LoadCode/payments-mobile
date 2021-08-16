ab4063cdfda917eb974609eb9d2cd16b4114ced1
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2021-08-16 15:03:50 -0500

    Settings for babel and typescript module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.ts',
+          '.android.tsx',
+          '.ts',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@app': './src/app',
+          '@assets': './src/app/assets',
+          '@components': './src/app/components',
+          '@constants': './src/app/constants',
+          '@screens': './src/app/screens',
+          '@interfaces': './src/interfaces',
+          '@redux': './src/redux',
+        },
+      },
+    ],
+  ],
 };
